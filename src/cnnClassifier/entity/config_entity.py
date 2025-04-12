78d7e95c5a621bc21dbcff67750edbbf670f2c04
Ankit Kaswan <ankit.iitd2014@gmail.com>
2025-04-12 19:52:00 +0530

    Added data transformation config

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -16,3 +16,10 @@ class DataValidationConfig:
     unzip_dir: str
     required_files: List[str]
 
+
+@dataclass
+class DataTransformationConfig:
+    train_dir: str
+    test_dir: str
+    image_size: tuple
+    batch_size: int
