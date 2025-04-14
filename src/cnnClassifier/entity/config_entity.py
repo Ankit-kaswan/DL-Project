a6170b196caf9f8fb326cd1585b726c9b866a1d7
Ankit Kaswan <ankit.iitd2014@gmail.com>
2025-04-14 20:15:27 +0530

    Added evaluation config

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -54,3 +54,13 @@ class ModelTrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
+    score_file_path: Path = Path("scores.json")  # Optional
